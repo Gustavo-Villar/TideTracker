ec1932bc2ca7bcadf13dcd9e34f4804309d103a5
Gustavo Villar <gustavohenriquevillar@gmail.com>
2024-02-17 15:42:33 -0300

    feat: get posts - Scrape XML - Background concurrency routine

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Gustavo-Villar/TideTracker/internal/database"
 	"github.com/go-chi/chi/v5"
@@ -36,9 +37,12 @@ func main() {
 		log.Fatal("Can't connect to database", err)
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
@@ -76,6 +80,5 @@ func main() {
 	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
-
 	}
 }
